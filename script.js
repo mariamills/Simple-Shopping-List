97be8e08afd4f5b26ae23e647be107280226a601
Maria Mills <maria@mariamills.org>
2023-01-08 08:13:44 -0500

    several modifications, see description
    -complete redesign for a more modern style and feel
    -added edit functionality
    -resolved difficulties with the functionality of marking complete & deleting
    -added a gear icon in anticipation of the implementation of the settings feature

--- script.js
+++ script.js
@@ -4,55 +4,99 @@ const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
 //Max length that the user's input can be
-let maxLength = 15;
+let maxLength = 25;
 
 //Event listener for add button
 addBtn.addEventListener("click", function () {
-  if (checkLength()) {
+  if (isValidLength(input.value)) {
     addToList();
+  } else {
+    input.classList.add("error");
+    alert(
+      "Please make sure your item is greater than 0 characters & less than 25 characters."
+    );
   }
 });
 
 //Event listener for Enter key pressed
-input.addEventListener("keypress", function (e) {
-  if (checkLength() > 0 && e.code === "Enter") {
-    addToList();
+input.addEventListener("keyup", function (e) {
+  if (e.code === "Enter") {
+    if (isValidLength(input.value)) {
+      addToList();
+    } else {
+      input.classList.add("error");
+      alert(
+        "Please make sure your item is greater than 0 characters & less than 25 characters."
+      );
+    }
   }
 });
 
 //Function declaration for checking input value length
-function checkLength() {
-  return input.value.length > 0 && input.value.length < maxLength;
+function isValidLength(input) {
+  const isGreaterThanZero = input.length > 0;
+  const isLessThanMax = input.length < maxLength;
+  return isGreaterThanZero && isLessThanMax;
 }
 
 //Function declaration for adding an item to the list
 function addToList() {
   let div = document.createElement("div");
   let li = document.createElement("li");
-  let i = document.createElement("i");
+  let i1 = document.createElement("i");
+  let i2 = document.createElement("i");
 
-  div.classList.add("list-item");
-  i.classList.add("fa-solid", "fa-trash");
+  i1.classList.add("fa-solid", "fa-pencil");
+  i2.classList.add("fa-solid", "fa-trash");
   li.innerText = input.value;
-  div.appendChild(li);
-  div.appendChild(i);
-  ul.appendChild(div);
+
+  ul.appendChild(li);
+  div.appendChild(i1);
+  div.appendChild(i2);
+  li.appendChild(div);
   input.value = "";
 }
 
 //Had to make use of event delegation since elements are created dynamically
 //Item cross-off functionality
 ul.addEventListener("click", function (e) {
-  if (e.target.tagName.toLowerCase() === "li") {
-    e.target.classList.toggle("done");
-    console.log("Completed!");
+  const target = e.target;
+
+  if (target.matches("li:not(i)")) {
+    target.classList.toggle("done");
   }
 });
 
 //Delete functionality
 ul.addEventListener("click", function (e) {
-  if (e.target.tagName.toLowerCase() === "i") {
-    e.target.parentElement.remove();
-    console.log("Removed");
+  if (e.target.classList.contains("fa-trash")) {
+    e.target.parentElement.parentElement.remove();
+  }
+});
+
+//Edit functionality
+ul.addEventListener("click", function (e) {
+  if (e.target.classList.contains("fa-pencil")) {
+    let parent = e.target.parentNode.parentNode; // Get the parent li element
+    let newInput = document.createElement("input");
+
+    // Replace the li element with the input element
+    parent.replaceChild(newInput, parent.firstChild);
+
+    newInput.addEventListener("keyup", function (e) {
+      if (e.key === "Enter") {
+        let newText = document.createTextNode(newInput.value);
+        if (isValidLength(newText)) {
+          parent.replaceChild(newText, newInput);
+        } else {
+          newInput.classList.add("error");
+          alert(
+            "Please make sure your item is greater than 0 characters & less than 25 characters."
+          );
+        }
+      }
+    });
+
+    newInput.focus();
   }
 });
